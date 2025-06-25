c79015595a61931838b5a435195e70e699c1a0c4
kshitijraj08 <kshitijraj@deloitte.com>
2025-06-25 14:07:13 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 public HelloWorld()
 
   {
-    
+     System.out.println("This is a constructor");
   }
   public static void main(String[] args) {
         System.out.println("Hello, World!");
